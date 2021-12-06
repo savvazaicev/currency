0c76a1c2c1de0c449b49178fe114074bb7df9bd0
Savva <oisurka@gmail.com>
2021-12-06 12:01:04 +0300

    add json_serializable

--- lib/data/currency_list/currency.dart
+++ lib/data/currency_list/currency.dart
@@ -1,3 +1,7 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'currency.g.dart';
+
 /// Cur_ID : 440
 /// Date : "2021-10-27T00:00:00"
 /// Cur_Abbreviation : "AUD"
@@ -5,53 +9,26 @@
 /// Cur_Name : "Австралийский доллар"
 /// Cur_OfficialRate : 1.8113
 
+@JsonSerializable()
 class Currency {
-  Currency({
-    int? curID,
-    String? date,
-    String? curAbbreviation,
-    int? curScale,
-    String? curName,
-    double? curOfficialRate,
-  }) {
-    _curID = curID;
-    _date = date;
-    _curAbbreviation = curAbbreviation;
-    _curScale = curScale;
-    _curName = curName;
-    _curOfficialRate = curOfficialRate;
-  }
+  Currency(this.curID, this.date, this.curAbbreviation, this.curScale,
+      this.curName, this.curOfficialRate);
 
-  Currency.fromJson(dynamic json) {
-    _curID = json['Cur_ID'];
-    _date = json['Date'];
-    _curAbbreviation = json['Cur_Abbreviation'];
-    _curScale = json['Cur_Scale'];
-    _curName = json['Cur_Name'];
-    _curOfficialRate = json['Cur_OfficialRate'];
-  }
-  int? _curID;
-  String? _date;
-  String? _curAbbreviation;
-  int? _curScale;
-  String? _curName;
-  double? _curOfficialRate;
+  @JsonKey(name: 'Cur_ID')
+  final int? curID;
+  @JsonKey(name: 'Date')
+  final String? date;
+  @JsonKey(name: 'Cur_Abbreviation')
+  final String? curAbbreviation;
+  @JsonKey(name: 'Cur_Scale')
+  final int? curScale;
+  @JsonKey(name: 'Cur_Name')
+  final String? curName;
+  @JsonKey(name: 'Cur_OfficialRate')
+  final double? curOfficialRate;
 
-  int? get curID => _curID;
-  String? get date => _date;
-  String? get curAbbreviation => _curAbbreviation;
-  int? get curScale => _curScale;
-  String? get curName => _curName;
-  double? get curOfficialRate => _curOfficialRate;
+  factory Currency.fromJson(Map<String, dynamic> json) =>
+      _$CurrencyFromJson(json);
 
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['Cur_ID'] = _curID;
-    map['Date'] = _date;
-    map['Cur_Abbreviation'] = _curAbbreviation;
-    map['Cur_Scale'] = _curScale;
-    map['Cur_Name'] = _curName;
-    map['Cur_OfficialRate'] = _curOfficialRate;
-    return map;
-  }
+  Map<String, dynamic> toJson() => _$CurrencyToJson(this);
 }
